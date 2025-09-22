b38541b06c1370297e49a32dd06d5328b91881d9
rosanadev <carvallhorosana@gmail.com>
2025-09-21 22:52:39 -0300

    feat(avaliacao): implementa lógica para criar avaliação

--- schedule/src/main/java/com/clinicaestetica/schedule/service/AvaliacaoService.java
+++ schedule/src/main/java/com/clinicaestetica/schedule/service/AvaliacaoService.java
@@ -1,9 +1,17 @@
 package com.clinicaestetica.schedule.service;
 
+import com.clinicaestetica.schedule.enums.StatusAgendamento;
+import com.clinicaestetica.schedule.model.Agendamento;
 import com.clinicaestetica.schedule.model.Avaliacao;
+import com.clinicaestetica.schedule.repository.AgendamentoRepository;
 import com.clinicaestetica.schedule.repository.AvaliacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.NoSuchElementException;
+
 
 @Service
 public class AvaliacaoService {
@@ -11,8 +19,21 @@ public class AvaliacaoService {
     @Autowired
     private AvaliacaoRepository avaliacaoRepository;
 
-    public Avaliacao criarAvaliacao(Avaliacao avaliacao) {
-        // Lógica de negócio para a avaliação (se houver)
+    @Autowired
+    private AgendamentoRepository agendamentoRepository;
+
+    public Avaliacao criarAvaliacao(Long agendamentoId, Avaliacao avaliacao) {
+        // Encontra o agendamento pelo ID
+        Agendamento agendamento = agendamentoRepository.findById(agendamentoId)
+                    .orElseThrow(() -> new NoSuchElementException("Agendamento não encontrado"));
+
+         // Verificar se o status do agendamento é CONCLUIDO
+        if (agendamento.getStatus() != StatusAgendamento.CONCLUÍDO) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não é possível avaliar um agendamento que não esteja concluído.");
+        }
+        
+        // Associar o agendamento à avaliação e salvar
+        avaliacao.setAgendamento(agendamento);
         return avaliacaoRepository.save(avaliacao);
+        }
     }
-}
