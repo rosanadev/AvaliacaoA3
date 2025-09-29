1c2aa3333cffce1ba0831a2bed9a69f398769acb
rosanadev <carvallhorosana@gmail.com>
2025-09-28 21:04:16 -0300

    feat(SolicitacaoController): adiciona endpoint GET /solicitacoes para o administrador

--- schedule/src/main/java/com/clinicaestetica/schedule/controller/SolicitacaoController.java
+++ schedule/src/main/java/com/clinicaestetica/schedule/controller/SolicitacaoController.java
@@ -3,6 +3,9 @@ package com.clinicaestetica.schedule.controller;
 import com.clinicaestetica.schedule.model.Solicitacao;
 import com.clinicaestetica.schedule.service.SolicitacaoService;
 import jakarta.validation.Valid;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/solicitacoes")
@@ -17,6 +21,12 @@ public class SolicitacaoController {
 
     @Autowired
     private SolicitacaoService solicitacaoService;
+
+    @GetMapping
+    public ResponseEntity<List<Solicitacao>> listarSolicitacoes () {
+        List<Solicitacao> solicitacoes = solicitacaoService.listarSolicitacoes();
+        return new ResponseEntity<>(solicitacoes, HttpStatus.OK);
+    }
 
     @PostMapping
     public ResponseEntity<Solicitacao> criarSolicitacao(@Valid @RequestBody Solicitacao solicitacao) {
