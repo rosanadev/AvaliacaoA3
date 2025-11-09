bb9953da2ad2f92b2ef6eb93432e6dce3414ee7c
rosanadev <carvallhorosana@gmail.com>
2025-11-09 01:20:57 -0300

    feat(SolicitacaoController): Adicionando novo endpoint post para receber o CriarSolicitacaoReagendamentoDTO

--- schedule/src/main/java/com/clinicaestetica/schedule/controller/SolicitacaoController.java
+++ schedule/src/main/java/com/clinicaestetica/schedule/controller/SolicitacaoController.java
@@ -1,9 +1,12 @@
 package com.clinicaestetica.schedule.controller;
 import com.clinicaestetica.schedule.dto.CriarSolicitacaoDTO;
+import com.clinicaestetica.schedule.dto.CriarSolicitacaoReagendamentoDTO;
 import com.clinicaestetica.schedule.model.Solicitacao;
 import com.clinicaestetica.schedule.service.SolicitacaoService;
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,5 +34,17 @@ public class SolicitacaoController {
     public ResponseEntity<Solicitacao> criarSolicitacao(@Valid @RequestBody CriarSolicitacaoDTO dto) {
         Solicitacao novaSolicitacao = solicitacaoService.criarSolicitacaoAgendamento(dto);
         return new ResponseEntity<>(novaSolicitacao, HttpStatus.CREATED);
+    } 
+    
+    @PostMapping("/reagendamento")
+    public ResponseEntity<Solicitacao> criarSolicitacaoReagendamento(@Valid @RequestBody CriarSolicitacaoReagendamentoDTO dto) {
+        try {
+            Solicitacao novaSolicitacao = solicitacaoService.criarSolicitacaoReagendamento(dto);
+            return new ResponseEntity<>(novaSolicitacao, HttpStatus.CREATED);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build(); 
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(null); 
+        }
     }
 }
