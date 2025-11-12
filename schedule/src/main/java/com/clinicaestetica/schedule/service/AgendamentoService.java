96154d52f2d7e304ee4ad8a06a46b82d4e10a5ef
Evely Paranhos <evelyparanhos05@gmail.com>
2025-11-11 21:26:38 -0300

    fix(AgendamentoRepository): Corrigindo conflito de horário

--- schedule/src/main/java/com/clinicaestetica/schedule/service/AgendamentoService.java
+++ schedule/src/main/java/com/clinicaestetica/schedule/service/AgendamentoService.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.math.RoundingMode;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- schedule/src/main/java/com/clinicaestetica/schedule/service/SolicitacaoService.java
+++ schedule/src/main/java/com/clinicaestetica/schedule/service/SolicitacaoService.java
@@ -17,6 +17,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.time.format.DateTimeFormatter;
 
 
 @Service
